64b9b51ece19b5b71ce1d6a19004d21144059750
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2021-02-02 22:40:28 +0100

    template functions

--- test/extract_mvs.c
+++ test/extract_mvs.c
@@ -241,25 +241,82 @@ void avcodec_register_all_w(){
   avcodec_register_all();
 }
 
-AVFormatContext* AVFormatContext_w() {
+AVFormatContext* init_AVFormatContext() {
   AVFormatContext * ret =NULL;
   return ret;
 }
 
-
-int avformat_open_input_w(char *filename, AVFormatContext *fmt_ctx){
-  return avformat_open_input(&fmt_ctx, filename, NULL, NULL);
-  // if (result < 0) {
-  //   fprintf(stderr, "Could not open source file %s\n", filename);
-  //   exit(1);
-  // }
-  // return fmt_ctx_v2;
+AVCodecContext* init_AVCodecContext(){
+  AVCodecContext* ret = NULL;
+  return ret;
 }
 
-void avformat_find_stream_info_w(AVFormatContext *fmt_ctx){
-  if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
-    fprintf(stderr, "Could not find stream information\n");
-    exit(1);
-  }
+AVStream* init_AVStream(){
+  AVStream* ret = NULL;
+  return ret;
 }
+
+AVFrame* init_AVFrame(){
+  AVFrame* ret = NULL;
+  return ret;
+}
+
+AVFormatContext* init_AVFormatContext() {
+
+}
+
+AVCodecContext* init_AVCodecContext() {
+
+}
+
+AVStream* init_AVStream() {
+
+}
+
+AVFrame* init_AVFrame() {
+
+}
+
+AVDictionary* init_AVDictionary() {
+
+}
+
+int avformat_open_input_w(char *filename, AVFormatContext *fmt_ctx) {
+
+}
+
+int avformat_find_stream_info_w(AVFormatContext *fmt_ctx) {
+
+}
+
+int av_find_best_stream_w(AVFormatContext *fmt_ctx, AVCodec *dec) {
+
+}
+
+AVCodecContext * avcodec_alloc_context3_w(AVCodec *dec) {
+
+}
+
+int avcodec_parameters_to_context(AVCodecContext *dec_ctx, AVStream* st) {
+
+}
+
+void av_dict_set_w(AVDictionary* opts) {
+
+}
+
+int avcodec_open2_w(AVCodecContext *dec_ctx, AVCodec *dec, AVDictionary *opts) {
+
+}
+
+AVFrame* av_frame_alloc_w(){
+
+}
+
+void close(AVCodecContext* video_dec_ctx, AVFormatContext *fmt_ctx, AVFrame* frame) {
+
+}
+
+AVStream* get_stream(AVFormatContext *fmt_ctx, int stream_idx) {
   
+}
--- test/extract_mvs.h
+++ test/extract_mvs.h
@@ -8,6 +8,21 @@ void extract_mvs(char *filename, int **out, int *out_dim1, int *out_dim2,
 
 void av_register_all_w();
 void avcodec_register_all_w();
-AVFormatContext* AVFormatContext_w();
+
+AVFormatContext* init_AVFormatContext();
+AVCodecContext* init_AVCodecContext();
+AVStream* init_AVStream();
+AVFrame* init_AVFrame();
+AVDictionary* init_AVDictionary();
+
 int avformat_open_input_w(char *filename, AVFormatContext *fmt_ctx);
-void avformat_find_stream_info_w(AVFormatContext *fmt_ctx);
+int avformat_find_stream_info_w(AVFormatContext *fmt_ctx);
+int av_find_best_stream_w(AVFormatContext *fmt_ctx, AVCodec *dec);
+AVCodecContext * avcodec_alloc_context3_w(AVCodec *dec);
+int avcodec_parameters_to_context(AVCodecContext *dec_ctx, AVStream* st);
+void av_dict_set_w(AVDictionary* opts);
+int avcodec_open2_w(AVCodecContext *dec_ctx, AVCodec *dec, AVDictionary *opts);
+AVFrame* av_frame_alloc_w();
+void close(AVCodecContext* video_dec_ctx, AVFormatContext *fmt_ctx, AVFrame* frame);
+
+AVStream* get_stream(AVFormatContext *fmt_ctx, int stream_idx);
